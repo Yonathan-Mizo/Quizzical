4d9fcaedf253575d329800264bb6631034fa57c2
Yonathan Mizo <ralteyonatan@yesmalot.co.il>
2022-10-30 21:15:26 +0200

    Updates

---
+++ precache-manifest.5c1ae973e845707ca97f402b5c16cf9e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6521b95c3e5639e612da",
+    "url": "/Quizzical/static/css/main.73a7186d.chunk.css"
+  },
+  {
+    "revision": "6521b95c3e5639e612da",
+    "url": "/Quizzical/static/js/main.6521b95c.chunk.js"
+  },
+  {
+    "revision": "ab2229c7a535dac44dd9",
+    "url": "/Quizzical/static/js/1.ab2229c7.chunk.js"
+  },
+  {
+    "revision": "8e2e41ffc7890a69b5e5",
+    "url": "/Quizzical/static/js/runtime~main.8e2e41ff.js"
+  },
+  {
+    "revision": "46a6424cefdfc980eca74acacae30ced",
+    "url": "/Quizzical/index.html"
+  }
+];
